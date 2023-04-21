ebf17707f2953701c5f053385263b4dd6736f48c
profullstackdeveloper <soldier9106@gmail.com>
2023-04-21 17:56:01 +0900

    update: content

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -9,14 +9,14 @@ function AboutCard() {
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
             Hi Everyone, I am <span className="purple">Tajnarine Ganpat </span>
-            from <span className="purple"> Minnesota, U.S.</span>
+            from <span className="purple"> Minnesota, United States.</span>
             <br />I am a senior Blockchain and Full Stack Developer.
             <br />
             <br />           
             University of Minnesota   
             <br />
            
-            <span className="purple">Master Degree</span> of Science and Engineering with <span className="purple">GPA 3.9</span>.
+            <span className="purple">Bachelors Degree</span> in Computer Science with <span className="purple">GPA 3.85</span>.
             <br />
             <br />      
             Apart from coding, some other activities that I love to do!
